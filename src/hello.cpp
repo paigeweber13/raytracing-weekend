7dd45c1d80f4f0352d4838c3489b2dff2dd2bb6e
Riley Weber <rileyw13@protonmail.com>
2021-02-12 17:26:58 -0500

    initial commit: started from yacs

---
+++ src/hello.cpp
@@ -0,0 +1,9 @@
+#include "hello.hpp"
+
+void sayHi() {
+  std::cout << "Hello, world!" << std::endl;
+}
+
+int gimmeFive() {
+  return 5;
+}
---
+++ src/hello.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void sayHi();
+
+int gimmeFive();
